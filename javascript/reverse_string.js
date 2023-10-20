25f24a321b7ad482fba1a18e21613f0b30e5f9e4
Kyra Hamerling-Potts <khamerling-potts@wustl.edu>
2023-10-20 10:09:36 -0400

    reverse string done

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,5 +1,9 @@
 function reverseString(str) {
-  // type your code here
+  let reversed = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+  return reversed;
 }
 
 if (require.main === module) {
@@ -11,9 +15,23 @@ if (require.main === module) {
 
   console.log("Expecting: 'ybabtac'");
   console.log("=>", reverseString("catbaby"));
+
+  console.log("expecting tunaep");
+  console.log(reverseString("peanut"));
+
+  console.log("expecting h");
+  console.log(reverseString("h"));
 }
 
 module.exports = reverseString;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+/* 
+Problem: given a string, write a function that returns a reversed copy of that string
+create new string to hold reversed copy
+loop through given string, starting at length-1 and going to 0
+concat string[i] onto new string
+return new string
+*/
